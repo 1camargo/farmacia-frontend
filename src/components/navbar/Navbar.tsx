8e358356981cf83ef67164053970711a9213f1ab
Gabriel Camargo <camargoxg@gmail.com>
2024-08-16 11:30:06 -0300

    :sparkles: feat: criação dos componentes e home

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+function Navbar() {
+
+  return (
+    <>
+     <div className='w-full bg-indigo-400 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Farmácia Rugged</Link>
+
+            <div className='flex gap-4'>
+            <div className='hover:underline'>Produtos</div>
+            <Link to='/temas' className='hover:underline'>Categorias</Link>
+              <Link to='cadastroTema' className='hover:underline'>Cadastrar categoria</Link>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
